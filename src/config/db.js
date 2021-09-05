8b7e93078c919691b95655209f2649d9d7bcff2d
Diego Lorenesi <drlorenesi@gmail.com>
2021-09-05 12:28:51 -0600

    Actualizar conexion a MongoDB.

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  await mongoose.connect(process.env.URL_MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  await mongoose.connect(process.env.URL_MONGO);
   console.log(
     `- Conectado a ${mongoose.connection.name} en ${mongoose.connection.host} como ${mongoose.connection.user}`
   );
