9782971527a65885ae9c4933a7101c0eec8adbe8
Diego Lorenesi <drlorenesi@gmail.com>
2021-09-19 20:17:12 -0600

    Actualizar db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
   await mongoose.connect(process.env.URL_MONGO);
   console.log(
-    `- Conectado a ${mongoose.connection.name} en ${mongoose.connection.host} como ${mongoose.connection.user}`
+    `- Conectado a ${mongoose.connection.name} en ${mongoose.connection.host}`
   );
 }
 
